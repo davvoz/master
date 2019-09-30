1e1c10bbab254be441e433db33a59b72fd4b33f1
davvoz <dav.baz@alice.it>
2019-09-30 13:36:07 +0200

    2 files modified, 1 added

---
+++ src/app/classes/piano-label.ts
@@ -0,0 +1,49 @@
+import { Coordinates } from '../interfaces/interfaces';
+import { Shape } from './shape';
+export class PianoLabel {
+  par: Coordinates;
+  collisions: number;
+  ctxGui:CanvasRenderingContext2D;
+  lato;
+  constructor(x: number, y: number, ctxGui: CanvasRenderingContext2D, par: Coordinates, collisions: number, color: string,lato:number) {
+    this.par = par;
+    this.collisions = collisions;
+    this.ctxGui = ctxGui;
+    this.lato = lato;
+  }
+  private getText() {
+    return this.par
+  }
+  private setColor() {
+  }
+  private getCollision() {
+    return this.collisions
+  }
+  public setCollisions(value: number) {
+    this.collisions = value;
+  }
+   draw() {
+    for (let y = 0; y < this.ctxGui.canvas.height; y = y + this.lato) {
+      for (let x = 0; x < this.ctxGui.canvas.width; x = x + this.lato) {
+        if (x % 8 == 0) {
+          this.ctxGui.beginPath();
+          this.ctxGui.moveTo(x, 0);
+          this.ctxGui.strokeStyle = "black";
+          this.ctxGui.lineTo(x, this.lato);
+          this.ctxGui.shadowBlur = 0;
+          this.ctxGui.stroke();
+        }
+        this.ctxGui.beginPath();
+        if (y % 8) {
+          this.ctxGui.fillStyle = "rgb(32,32,32)";
+        } else {
+          this.ctxGui.fillStyle = "rgb(40,40,40)";
+        }
+        this.ctxGui.strokeStyle = "rgb(24,24,24)";
+        this.ctxGui.rect(x, y, this.lato, this.lato);
+        this.ctxGui.fill()
+        this.ctxGui.stroke();
+      }
+    }
+  }
+}
--- src/app/piano-roll/piano-roll.component.ts
+++ src/app/piano-roll/piano-roll.component.ts
@@ -1,38 +1,68 @@
-import { Component, HostListener, ElementRef, ViewChild, AfterViewInit, NgZone } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { TimerService } from '../services/timer.service';
-import { SoundService } from '../services/sound.service';
-import { RadioBtn, Adsr } from '../interfaces/interfaces';
-import { Square } from '../classes/square';
-import { UserGui } from '../classes/user-gui';
-import { Cursor } from '../classes/cursor';
-import { LineOfSquares } from '../classes/line-of-squares';
-import { Coordinates, Collision } from '../interfaces/interfaces';
-import { ChannelMergerService } from '../services/channel-merger.service';
-
+import {
+  Component,
+  HostListener,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+  NgZone
+} from "@angular/core";
+import { Subscription } from "rxjs";
+import { TimerService } from "../services/timer.service";
+import { SoundService } from "../services/sound.service";
+import { RadioBtn, Adsr } from "../interfaces/interfaces";
+import { Square } from "../classes/square";
+import { UserGui } from "../classes/user-gui";
+import { Cursor } from "../classes/cursor";
+import { LineOfSquares } from "../classes/line-of-squares";
+import { Coordinates, Collision } from "../interfaces/interfaces";
+import { ChannelMergerService } from "../services/channel-merger.service";
 
 @Component({
-  selector: 'app-piano-roll',
-  templateUrl: './piano-roll.component.html',
-  styleUrls: ['./piano-roll.component.css'],
+  selector: "app-piano-roll",
+  templateUrl: "./piano-roll.component.html",
+  styleUrls: ["./piano-roll.component.css"],
   host: {
-    '(document:keypress)': 'handleKeyboardEvent($event)'
+    "(document:keypress)": "handleKeyboardEvent($event)"
   }
 })
 @Component({
-  selector: 'app-piano-roll',
-  templateUrl: './piano-roll.component.html',
-  styleUrls: ['./piano-roll.component.css']
+  selector: "app-piano-roll",
+  templateUrl: "./piano-roll.component.html",
+  styleUrls: ["./piano-roll.component.css"]
 })
 export class PianoRollComponent implements AfterViewInit {
-
-  freq = [110.00, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.0, 466.16, 493.88, 523.25];
+  freq = [
+    110.0,
+    116.54,
+    123.47,
+    130.81,
+    138.59,
+    146.83,
+    155.56,
+    164.81,
+    174.61,
+    185.0,
+    196.0,
+    207.65,
+    277.18,
+    293.66,
+    311.13,
+    329.63,
+    349.23,
+    369.99,
+    392.0,
+    415.3,
+    440.0,
+    466.16,
+    493.88,
+    523.25
+  ];
   key;
-  @HostListener('document:keypress', ['$event'])
+  @HostListener("document:keypress", ["$event"])
   handleKeyboardEvent(event: KeyboardEvent) {
     this.key = event.key;
   }
-  title = 'OSCILLATOR';
+  title = "OSCILLATOR";
 
   isPlayed = false;
   subscription: Subscription;
@@ -48,61 +78,101 @@ export class PianoRollComponent implements AfterViewInit {
   isMuted = 0;
   filterCutoff = 0;
   filterReso = 0;
-  lfoWaveSelected = 'sine';
+  lfoWaveSelected = "sine";
   lfoAmplitude = 100;
   lfoRate = 0;
 
   coord: Coordinates = { x: 0, y: 0 };
-  @ViewChild('canvas', { static: false }) canvas: ElementRef<HTMLCanvasElement>;
-  @ViewChild('canvasGui', { static: false }) canvasGui: ElementRef<HTMLCanvasElement>;
-  @ViewChild('canvasLine', { static: false }) canvasLine: ElementRef<HTMLCanvasElement>;
+  @ViewChild("canvas", { static: false }) canvas: ElementRef<HTMLCanvasElement>;
+  @ViewChild("canvasGui", { static: false }) canvasGui: ElementRef<
+    HTMLCanvasElement
+  >;
+  @ViewChild("canvasLine", { static: false }) canvasLine: ElementRef<
+    HTMLCanvasElement
+  >;
+  @ViewChild("pianoLabelCanvas", { static: false })
+  pianoLabelCanvas: ElementRef<HTMLCanvasElement>;
   private ctxGui: CanvasRenderingContext2D;
   private ctx: CanvasRenderingContext2D;
   private ctxLine: CanvasRenderingContext2D;
+  private ctxPianoLabel: CanvasRenderingContext2D;
 
   userGui: UserGui;
   enemies: Square[] = [];
   myLine: LineOfSquares;
-  waveSelected = 'square';
-  filterSelected = 'allpass';
-  waveforms = ['square', 'sine', 'sawtooth', 'triangle'];
-  filterType = ['lowpass', 'highpass', 'bandpass', 'lowshelf', 'peaking', 'notch', 'allpass']
+  pianoLabel:PianoLabel;
+  waveSelected = "square";
+  filterSelected = "allpass";
+  waveforms = ["square", "sine", "sawtooth", "triangle"];
+  filterType = [
+    "lowpass",
+    "highpass",
+    "bandpass",
+    "lowshelf",
+    "peaking",
+    "notch",
+    "allpass"
+  ];
 
-  constructor(public myTimer: TimerService, public mySound: SoundService ,public myChannelMerger :ChannelMergerService) {
+  constructor(
+    public myTimer: TimerService,
+    public mySound: SoundService,
+    public myChannelMerger: ChannelMergerService
+  ) {
     this.coord.x = 0;
     this.coord.y = 0;
     this.myTimer.speed = 180;
-
   }
   ngAfterViewInit() {
     this.ctxGui = this.canvasGui.nativeElement.getContext("2d");
     this.ctx = this.canvas.nativeElement.getContext("2d");
     this.ctxLine = this.canvasLine.nativeElement.getContext("2d");
+    this.ctxPianoLabel = this.pianoLabelCanvas.nativeElement.getContext("2d");
 
-    this.myLine = new LineOfSquares(this.lato, -1, 0, '0,0,0', this.ctxLine, 100, 24, 'VERTICALE');
+    this.myLine = new LineOfSquares(
+      this.lato,
+      -1,
+      0,
+      "0,0,0",
+      this.ctxLine,
+      100,
+      24,
+      "VERTICALE"
+    );
     this.myLine.standUp();
 
     this.populateEnemiesArray();
     this.standUpEnemies();
 
-    this.userGui = new UserGui(0, 0, this.ctxGui, { x: 0, y: 0 }, 0, '0,0,0', this.lato);
+    this.userGui = new UserGui(
+      0,
+      0,
+      this.ctxGui,
+      { x: 0, y: 0 },
+      0,
+      "0,0,0",
+      this.lato
+    );
     this.userGui.draw();
 
-    this.subscription = this.myTimer.trackStateItem$
-      .subscribe(res => {
-        if (res.traksAreOn) {
-          if (res.isStarted) {
-            this.tick();
-          }
+    this.subscription = this.myTimer.trackStateItem$.subscribe(res => {
+      if (res.traksAreOn) {
+        if (res.isStarted) {
+          this.tick();
         }
-
-      });
+      }
+    });
   }
 
   tick() {
-    this.ctxLine.clearRect(0, 0, this.ctxLine.canvas.width, this.ctxLine.canvas.height);
+    this.ctxLine.clearRect(
+      0,
+      0,
+      this.ctxLine.canvas.width,
+      this.ctxLine.canvas.height
+    );
     this.coord = { x: this.myLine.getX(), y: 0 };
-    this.myLine.setColor('200,0,0');
+    this.myLine.setColor("200,0,0");
     const col: Collision = this.collisionsArrayControl(this.myLine);
     if (this.myLine.getX() == 31) {
       this.myLine.setX(0);
@@ -129,7 +199,9 @@ export class PianoRollComponent implements AfterViewInit {
     this.mySound.sustainVal = this.sustainVal;
     this.mySound.relase = this.relase;
     this.mySound.waveform = this.waveSelected;
-    this.isMuted == 0 ? this.mySound.muted = false : this.mySound.muted = true;
+    this.isMuted == 0
+      ? (this.mySound.muted = false)
+      : (this.mySound.muted = true);
     this.mySound.gain = this.gain;
     this.mySound.filterCutoff = this.filterCutoff;
     this.mySound.filterReso = this.filterReso;
@@ -142,16 +214,16 @@ export class PianoRollComponent implements AfterViewInit {
   }
   private populateEnemiesArray(): void {
     for (let i = 0; i < this.ctx.canvas.width / this.lato; i++) {
-      this.enemies.push(new Square(this.lato, i, 0, '100,100,100', this.ctx, 0));
+      this.enemies.push(
+        new Square(this.lato, i, 0, "100,100,100", this.ctx, 0)
+      );
     }
-
   }
 
   private standUpEnemies(): void {
     for (let i = 0; i < this.enemies.length; i++) {
       this.enemies[i].standUp();
       this.enemies[i].kill();
-
     }
   }
 
@@ -160,11 +232,11 @@ export class PianoRollComponent implements AfterViewInit {
     for (let i = 0; i < this.enemies.length; i++) {
       if (this.collision(square, this.enemies[i])) {
         //square.setColor(this.randomColorString());
-        return { esito: false, indice: i }
+        return { esito: false, indice: i };
       }
       count = i;
     }
-    return { esito: true, indice: count }
+    return { esito: true, indice: count };
   }
 
   public start() {
@@ -174,19 +246,19 @@ export class PianoRollComponent implements AfterViewInit {
 
   public stop() {
     this.isPlayed = false;
-    this.myTimer.stop()
+    this.myTimer.stop();
   }
 
   public pause() {
     this.isPlayed = false;
-    this.myTimer.pause()
+    this.myTimer.pause();
   }
 
   getColor() {
     if (this.isMuted) {
-      return 'red'
+      return "red";
     } else {
-      return 'green'
+      return "green";
     }
   }
 
@@ -198,19 +270,30 @@ export class PianoRollComponent implements AfterViewInit {
     };
   }
   private randomColorString() {
-    return this.getRandomInt(255, 0) + ',' + this.getRandomInt(255, 0) + ',' + this.getRandomInt(255, 0);
+    return (
+      this.getRandomInt(255, 0) +
+      "," +
+      this.getRandomInt(255, 0) +
+      "," +
+      this.getRandomInt(255, 0)
+    );
   }
   private getRandomInt(maxValue: number, minValue: number) {
     return Math.floor(Math.random() * (maxValue - 0)) + minValue; //Il max è escluso e il min è incluso
   }
   private collision(player: Square, enemy: Square) {
-    var distX = Math.abs(player.getX() - enemy.getX() - enemy.getDimensioneLato());
+    var distX = Math.abs(
+      player.getX() - enemy.getX() - enemy.getDimensioneLato()
+    );
     var dx = distX - enemy.getDimensioneLato();
-    return (dx == 0);
+    return dx == 0;
   }
   handleChange(evt) {
     let coo: Coordinates = this.getMousePos(evt);
-    this.enemies[coo.x].isStanding() ? this.enemies[coo.x].kill() : (this.enemies[coo.x].setTune(this.freq[coo.y]), this.enemies[coo.x].setY(coo.y - 1), this.enemies[coo.x].standUp());
+    this.enemies[coo.x].isStanding()
+      ? this.enemies[coo.x].kill()
+      : (this.enemies[coo.x].setTune(this.freq[coo.y]),
+        this.enemies[coo.x].setY(coo.y - 1),
+        this.enemies[coo.x].standUp());
   }
-
 }
